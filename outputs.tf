4d5d105bb6dcc4fe1466944a31d75436bc25943c
riqardos <kastiak.richard@gmail.com>
2023-02-14 11:04:31 +0100

    fix * notation to brackets

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "source_backup_vault_id" {
-  value       = aws_backup_vault.source.*.id
+  value       = aws_backup_vault.source[0].id
   description = "Backup Vault ID of source backup vault"
 }
 
 output "source_backup_vault_arn" {
-  value       = aws_backup_vault.source.*.arn
+  value       = aws_backup_vault.source[0].arn
   description = "Backup Vault ARN of source backup vault"
 }
 
 output "target_backup_vault_id" {
-  value       = aws_backup_vault.target.*.id
+  value       = try(aws_backup_vault.target[0].id, "")
   description = "Backup Vault ID of target backup vault"
 }
 
 output "target_backup_vault_arn" {
-  value       = aws_backup_vault.target.*.arn
+  value       = try(aws_backup_vault.target[0].arn, "")
   description = "Backup Vault ARN of target backup vault"
 }
